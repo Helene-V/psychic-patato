15e7957cbe88b173fad31359d1cafe8cb260b271
Ln <rainbowliketravel@gmail.com>
2021-12-14 22:21:33 +0100

    Compteur

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <Compteur />
+  </div>
+</template>
+
+<script>
+import Compteur from '@/components/Compteur.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Compteur
+  }
+}
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Compteur />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Compteur from '@/components/Compteur.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld, Compteur
+  }
+}
+</script>
